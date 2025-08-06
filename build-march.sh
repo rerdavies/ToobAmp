2f7a15bdf4b99359510abc5ecac9318a49dca931
Robin E. R. Davies <rerdavies@gmail.com>
2025-08-06 15:02:41 -0400

    Muti-arch build

---
+++ build-march.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Build aarch64 A72+A76 package for production.
+set -e
+rm -rf build
+rm -rf build-a76
+
+# Build for ToobAmp-a76.so
+mkdir -p build-a76
+cd build-a76
+cmake .. -D CMAKE_BUILD_TYPE=Release  -D A76_OPTIMIZATION=ON -D TOOB_MULTI_ARCH_BUILD=OFF -D CMAKE_VERBOSE_MAKEFILE=ON $@
+cd ..
+
+cmake --build ./build-a76 --target ToobAmpArch  --config Release 
+
+
+# Configure main build to do an a72+a76 build
+mkdir -p build
+cd build
+cmake .. -D CMAKE_BUILD_TYPE=Release  -D A72_OPTIMIZATION=ON -D TOOB_MULTI_ARCH_BUILD=ON -D CMAKE_VERBOSE_MAKEFILE=ON $@
+cd ..
+
+cmake --build ./build --target all --config Release 
+
+
+
+# build the package.
+./makePackage.sh
+
+
+
+#restore main build options.
+# Configure main build to do an a72+a76 build
+#mkdir -p build
+#cd build
+#cmake .. -D CMAKE_BUILD_TYPE=Release  -D TOOB_MULTI_ARCH_BUILD=OFF -D CMAKE_VERBOSE_MAKEFILE=ON $@
+#cd ..
+
