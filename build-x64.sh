6d5f1adde1bed39f98c2d466e08ad1c3f0a5f812
Robin E. R. Davies <rerdavies@gmail.com>
2025-09-06 07:38:01 -0400

    Use ninja cmake for build-x64.sh

--- build-x64.sh
+++ build-x64.sh
@@ -7,8 +7,8 @@ rm -rf build
 # Build for ToobAmp-a76.so
 mkdir -p build
 cd build
-cmake .. -D CMAKE_BUILD_TYPE=Release  -D CMAKE_VERBOSE_MAKEFILE=ON $@
+cmake .. -D CMAKE_BUILD_TYPE=Release  -D CMAKE_VERBOSE_MAKEFILE=ON -G Ninja $@
 cd ..
 
-cmake --build ./build --target all  --config Release 
+time cmake --build ./build --target all  --config Release -- -j 3
 
