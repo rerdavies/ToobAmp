b3e41c53f875d391bcd2b8f831f8a913618fb7f2
Robin E. R. Davies <rerdavies@gmail.com>
2025-09-09 20:40:26 -0400

    aarch64 sync

--- build-march.sh
+++ build-march.sh
@@ -7,7 +7,7 @@ rm -rf build-a76
 # Build for ToobAmp-a76.so
 mkdir -p build-a76
 cd build-a76
-cmake .. -D CMAKE_BUILD_TYPE=Release  -D A76_OPTIMIZATION=ON -D TOOB_MULTI_ARCH_BUILD=OFF -D CMAKE_VERBOSE_MAKEFILE=ON -G Ninja $@
+cmake .. -D CMAKE_BUILD_TYPE=Release  -D A76_OPTIMIZATION=ON -D TOOB_MULTI_ARCH_BUILD=OFF -D CMAKE_VERBOSE_MAKEFILE=ON -G Ninja 
 cd ..
 
 time cmake --build ./build-a76 --target ToobAmpArch  --config Release -- -j 6
@@ -16,7 +16,7 @@ time cmake --build ./build-a76 --target ToobAmpArch  --config Release -- -j 6
 # Configure main build to do an a72+a76 build
 mkdir -p build
 cd build
-cmake .. -D CMAKE_BUILD_TYPE=Release  -D A72_OPTIMIZATION=ON -D TOOB_MULTI_ARCH_BUILD=ON -D CMAKE_VERBOSE_MAKEFILE=ON -G Ninja  $@
+cmake .. -D CMAKE_BUILD_TYPE=Release  -D A72_OPTIMIZATION=ON -D TOOB_MULTI_ARCH_BUILD=ON -D CMAKE_VERBOSE_MAKEFILE=ON -G Ninja  
 cd ..
 
 time cmake --build ./build --target all --config Release -- -j 6
